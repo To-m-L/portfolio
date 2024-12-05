b178a233270b8526418291db756b2c2247f8fc08
To-m-L <tom8731@gmail.com>
2024-12-05 13:20:39 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="bg-gray-50 text-gray-900">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () => (
+  <header className="bg-blue-700 text-white py-6 px-4 text-center">
+    <h1 className="text-4xl font-bold">Tom Lin</h1>
+    <p className="mt-2">
+      <a href="mailto:tomlin8731@gmail.com" className="underline">
+        tomlin8731@gmail.com
+      </a>{" "}
+      •{" "}
+      <a href="https://linkedin.com/in/tom-lin-2b73981a2/" className="underline">
+        LinkedIn
+      </a>{" "}
+      •{" "}
+      <a href="https://github.com/To-m-L" className="underline">
+        GitHub
+      </a>{" "}
+      • 226-600-1892
+    </p>
+  </header>
+);
+
+export default Header;
